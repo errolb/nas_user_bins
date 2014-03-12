c5ccb5cb3d4338d8c7ea10326d014ebba77b8f01
errolb <errolburger1@gmail.com>
2014-03-12 11:50:07 +0200

    Move symlink location to /home/<user> dir

--- media_consolidator_syml.sh
+++ media_consolidator_syml.sh
@@ -3,13 +3,13 @@
 # This script fills a directory with a bunch of symlinks
 # from all the tv shows on my various media drives
 
-rm -f /media/media00/.all_media_symlinks/tvshows_live_action/*
+rm -f $HOME/.all_media_symlinks/tvshows_live_action/*
 
 for i in /media/media0[0-3]/entertainment/tv\ shows/live\ action/* ; do
     
     # changing spaces and parenthesis to underscores for symlink
     linkname=$(echo $(basename "$i") | sed -re 's/\s/_/g;s/[(]//g;s/[)]//g')
-    linkpath="/media/media00/.all_media_symlinks/tvshows_live_action/$linkname"
+    linkpath="$HOME/.all_media_symlinks/tvshows_live_action/$linkname"
     
     ln -s "$i" $linkpath
 
