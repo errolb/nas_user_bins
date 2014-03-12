daaadbbf8326cc17549ce28cd65772805746a8e7
errolb <errolburger1@gmail.com>
2014-03-12 12:56:20 +0200

    Added dynamic range for iterator

--- media_consolidator_syml.sh
+++ media_consolidator_syml.sh
@@ -3,9 +3,13 @@
 # This script fills a directory with a bunch of symlinks
 # from all the tv shows on my various media drives
 
+# clean dir
 rm -f $HOME/.all_media_symlinks/tvshows_live_action/*
 
-for i in /media/media0[0-3]/entertainment/tv\ shows/live\ action/* ; do
+# count the number of media drives available. Zero indexed.
+count_hdd=$( expr $(ls -l /media | grep "media0*" | wc -l) - 1)
+
+for i in /media/media0[0-$count_hdd]/entertainment/tv\ shows/live\ action/* ; do
     
     # changing spaces and parenthesis to underscores for symlink
     linkname=$(echo $(basename "$i") | sed -re 's/\s/_/g;s/[(]//g;s/[)]//g')
