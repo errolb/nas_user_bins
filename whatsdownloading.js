1047f56e6548c9efdb25645148af57cf74d75ac2
errolb <errolburger1@gmail.com>
2014-08-17 16:28:28 +0200

    clean up

--- whatsdownloading.js
+++ whatsdownloading.js
@@ -6,31 +6,35 @@ var fs = require('fs'),
 
 // base globals
 var username = process.env.USER;	
-var watchFolder = '/home/' + username + '/Dropbox/_global/_torrent_watch',
-	incompleteDir ='/media/media02/downloads/incomplete';
-var	sabNzbdApiKey = 'f28203358c8d11803afdc2e136414848';	
-	sabNzbdUrl = 'http://localhost:8080/api?mode=qstatus&output=json&apikey=' + 
-						sabNzbdApiKey;
-//var	realSAB = 'localhost:9091/sabNzbd/web';
+var watchFolder = '/home/' + username + '/Dropbox/_global/_torrent_watch';
+var	incompleteDir ='/media/media02/downloads/incomplete';
+var sabNzbdUrl = 'http://localhost:8080/api?mode=qstatus&output=json&apikey='; 
+var listName = 'downloading.md';
+var completePath 	= watchFolder + '/' + listName;
+var ignoreList = ['.DS_Store','._.DS_Store'];
 
-function processSites(url1, callback) {
-	fs.readdir(watchFolder, function(err, files) {
+function processSites(callback) {
+	fs.readFile('secretSabnzbdKey', 'utf8', function(err, data) {
 		if (err) throw err;
-		request(url1, function(error, response, body1) {
-			if (!error && response.statusCode == 200) {
-						fs.readdir(incompleteDir, function(err, files) {
-							if (err) throw err;
-							callback(body1, files);
-						});
-			}
+		fs.readdir(watchFolder, function(err, files) {
+			if (err) throw err;
+			var fullUrl = sabNzbdUrl + data;
+			fullUrl =  fullUrl.replace(/(\r\n|\n|\r)/gm,'');
+			console.log(fullUrl);
+			request(fullUrl, function(error, response, body1) {
+				if (!error && response.statusCode == 200) {
+					fs.readdir(incompleteDir, function(err, files) {
+						if (err) throw err;
+						callback(body1, files);
+					});
+				}
+			});
 		});
 	});
 }
-processSites(sabNzbdUrl,function(body1, files){
+processSites(function(body1, files){
 	body1 = JSON.parse(body1);
-	var list_name = 'downloading.md';
-	var complete_path 	= '/home/' + username + '/Dropbox/_global/_torrent_watch/' + list_name;
-	var ignore_list = [list_name, '.DS_Store','._.DS_Store'];
+	console.log(body1);
 	
 	// write to SABnzbd
 	var markdown = "# Downloading\n\n" + "## SABnzbd `( " + body1.jobs.length + " )`\n\n" + 
@@ -50,7 +54,7 @@ processSites(sabNzbdUrl,function(body1, files){
 					"--- | --- \n";
 
 	for (var i = 0; i <= files.length - 1; i++) {
-		if (!(ignore_list.indexOf(files[i]) > -1)) markdown = markdown + 
+		if (!(ignoreList.indexOf(files[i]) > -1)) markdown = markdown + 
 											files[i] + " | " + i + "\n";
 	}
 
@@ -59,7 +63,7 @@ processSites(sabNzbdUrl,function(body1, files){
 	markdown = markdown + "\n\n" + "This file was written on ** " + 
 			   time_written + " **";
 
-	fs.writeFile(complete_path, markdown, function(err){ 
+	fs.writeFile(completePath, markdown, function(err){ 
 		if (err) throw err;
 		console.log('Downloading list updated.');
 	});
