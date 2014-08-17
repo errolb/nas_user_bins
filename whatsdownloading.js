ccf7cdf3ae52fe1c46a1b6ed2ad299c04c60998e
errolb <errolburger1@gmail.com>
2014-08-17 16:01:39 +0200

    working

--- whatsdownloading.js
+++ whatsdownloading.js
@@ -2,38 +2,31 @@
 
 var fs = require('fs'),
 	moment = require('moment'),
-	async = require('async'),
-	request = require('request'),
-	cheerio = require('cheerio');
+	request = require('request');
 
 // base globals
 var username = process.env.USER;	
 var watchFolder = '/home/' + username + '/Dropbox/_global/_torrent_watch',
 	incompleteDir ='/media/media02/downloads/incomplete';
-
 var	transmissionApiKey = 'f28203358c8d11803afdc2e136414848';	
-	transmissionUrl = 'http://localhost:8080/api?mode=qstatus&output=json&apikey=' 
-					+ transmissionApiKey,
-	sabNzbdUrl = 'http://localhost:8080/';
+	transmissionUrl = 'http://localhost:8080/api?mode=qstatus&output=json&apikey=' + 
+						transmissionApiKey;
+var	realSAB = 'localhost:9091/transmission/web';
 
-function processSites(url1, url2, callback) {
+function processSites(url1, callback) {
 	fs.readdir(watchFolder, function(err, files) {
 		if (err) throw err;
 		request(url1, function(error, response, body1) {
 			if (!error && response.statusCode == 200) {
-				request(url2, function(error, response, body2) {
-					if (!error && response.statusCode == 200) {
 						fs.readdir(incompleteDir, function(err, files) {
 							if (err) throw err;
-							callback(body1, body2, files)
+							callback(body1, files);
 						});
-					}
-				});
 			}
 		});
 	});
 }
-processSites(transmissionUrl,sabNzbdUrl, function(body1, body2, files){
+processSites(transmissionUrl,function(body1, files){
 	body1 = JSON.parse(body1);
 	var list_name = 'downloading.md';
 	var complete_path 	= '/home/' + username + '/Dropbox/_global/_torrent_watch/' + list_name;
@@ -44,11 +37,11 @@ processSites(transmissionUrl,sabNzbdUrl, function(body1, body2, files){
 					"Filename | MB left | MB | Time left\n" + 
 					"--- | --- | --- | ---\n";
 
-	for (i in body1.jobs) {
-		markdown = markdown + body1.jobs[i].filename + " | " +
-				Math.round(body1.jobs[i].mbleft) + " | " +
-				Math.round(body1.jobs[i].mb) + " | " +
-				body1.jobs[i].timeleft + "\n";
+	for (var x in body1.jobs) {
+		markdown = markdown + body1.jobs[x].filename + " | " +
+				Math.round(body1.jobs[x].mbleft) + " | " +
+				Math.round(body1.jobs[x].mb) + " | " +
+				body1.jobs[x].timeleft + "\n";
 	}
 
 	// write incomplete transmission files	
--- whatsfinished.js
+++ whatsfinished.js
@@ -6,14 +6,14 @@ var async = require('async');
 
 // base globals
 var username = process.env.USER;	
-var watch_folder = '/home/' + username + '/Dropbox/_global/_torrent_watch'
+var watch_folder = '/home/' + username + '/Dropbox/_global/_torrent_watch';
 var complete_dir = '/media/media02/downloads/complete';
 
 // move all *.added files in the _watch directory to the added directory
 fs.readdir(watch_folder, function(err, files) {
 	var added_torrents = (function(){
 		var list = [];
-		for (i in files) {
+		for (var i in files) {
 			if (files[i].match(/\.added$/)) list.push(files[i]);
 		}
 		return list;
@@ -41,10 +41,10 @@ fs.readdir(complete_dir, function(err, files){
 	if (err) throw err;
 	
 	var list_name = 'completed.md';
-	var ignore_list = [list_name, '.DS_Store','._.DS_Store','downloading.md'];
+	var ignore_list = [list_name, '.DS_Store','._.DS_Store','downloading.md','movielist.md'];
 	var complete_path 	= '/home/' + username + '/Dropbox/_global/_torrent_watch/' + list_name;
 	var markdown = "# Completed Files\n\n";
-	var markdown = markdown + "---\n\n";
+	markdown = markdown + "---\n\n";
 
 	for (var i = 0; i <= files.length - 1; i++) {
 		if (!(ignore_list.indexOf(files[i]) > -1)) markdown = markdown + " - " + files[i] + "\n";
