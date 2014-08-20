3a4fdd649dc6fda4ce8bd74de8fc4251bd0a032a
errolb <errolburger1@gmail.com>
2014-08-20 11:52:30 +0200

    added music

--- media_consolidator_syml.sh
+++ media_consolidator_syml.sh
@@ -96,3 +96,23 @@ done
 # add spaces and parethesis wrapped dates for better media scraping
 cd $HOME/.all_media_symlinks/tvshows_anime
 rename 's/_/ /g;s/(\d\d\d\d$)/\($1\)/' *
+
+# ******************** MUSIC  ********************* #
+
+# clean dir
+rm -f $HOME/.all_media_symlinks/music/*
+
+for i in /media/media0[0-$count_hdd]/entertainment/music/* ; do
+    
+    # changing spaces and parenthesis to underscores for symlink
+	# as I couldn't get the ln to play nice with spaces
+    linkname=$(echo $(basename "$i") | sed -re 's/\s/_/g;s/[(]//g;s/[)]//g')
+    linkpath="$HOME/.all_media_symlinks/music/$linkname"
+    
+    ln -s "$i" $linkpath
+
+done
+
+# add spaces and parethesis wrapped dates for better media scraping
+cd $HOME/.all_media_symlinks/music
+rename 's/_/ /g;s/(\d\d\d\d$)/\($1\)/' *
