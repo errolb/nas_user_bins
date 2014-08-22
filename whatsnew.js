ec135add02480e5bcbc26da530544f3ccaafd329
errolb <errolburger1@gmail.com>
2014-08-22 21:43:44 +0200

    removed comments

--- whatsnew.js
+++ whatsnew.js
@@ -10,15 +10,11 @@ var username = process.env.USER;
 var writeFolder = '/home/' + username + '/Dropbox/_global/_torrent_watch';
 var listName = 'whatsnew.md';
 var completePath = writeFolder + '/' + listName;
+var newLine = args[0] + ' ' +args[1] + ' ' +args[2] + ' ' +args[3] + ' ' +
+			args[4] + ' ' +args[5]; 
 
-// accept stdin
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-process.stdin.on('data', function(data) {
-	  process.stdout.write(data);
-	  
-	  fs.appendFile(completePath, data, function (err) {
-		    if (err) throw err;
-			  console.log('Sickbeard data appended to: ' + completePath);
-	  });
+fs.appendFile(completePath, newLine, function (err) {
+	if (err) throw err;
+		console.log('Sickbeard data appended to: ' + completePath);
 });
+
