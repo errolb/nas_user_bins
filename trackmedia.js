137c0ba0c58d0e66eb046ae32894395976adfc80
errolb <errolburger1@gmail.com>
2014-09-12 21:39:14 +0200

    added functionality for multiple folders

---
+++ trackmedia.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+var fs = require('fs'),
+	moment = require('moment'),
+	async = require('async');
+
+// base globals
+var username = process.env.USER;	
+var listsDir = '/home/' + username + '/Dropbox/_global/_live_lists/',
+	torrentsComplete = '/media/media02/downloads/complete/',
+	ignorelist = [ '.DS_Store','._.DS_Store' ];
+
+// rtorrent
+fs.readdir(torrentsComplete, function(err, folders) {
+	if (err) throw err;
+	var listName = 'completed.md',
+		md = '',
+		title = '# Live Complete List',
+		timeWritten = 'Updated: __' + moment().format('MMMM Do YYYY, h:mm:ss a') + '__';
+
+	md = md +
+		title + '\n\n' +
+		timeWritten + '\n\n';
+
+	fs.writeFile(listsDir + listName, md, function(err) {
+		if (err) throw err;
+		async.each(folders, function(folder, callback) {
+			if (!( ignorelist.indexOf(folder) > -1 )) {
+				fs.readdir(torrentsComplete + folder + '/', function(err, files) {
+					md = '';
+					md = md +
+						'\n## ' + folder + '\n\n' +
+						'~ | Title\n' +
+						'- | -----\n';
+
+					for (var i in files) {
+						md = md +
+							i + ' | ' +
+							files[i] + '\n';
+					}
+					
+					fs.appendFile(listsDir + listName, md, function(err) {
+						if (err) throw err;
+						callback();
+					});
+				});
+			}
+		}, function(err) {
+			if (err) throw err;	
+		});
+	});
+});
+
