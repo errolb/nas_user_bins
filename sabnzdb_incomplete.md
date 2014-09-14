30bf42c9a76f7d8b03daa9cb5b6f52ec33471f72
errolb <errolburger1@gmail.com>
2014-09-14 16:46:20 +0200

    consolidate all js scripts

---
+++ sabnzdb_incomplete.md
@@ -0,0 +1,31 @@
+# Downloading
+
+## SABnzbd `( 25 )`
+
+~ | Filename | MB left | MB | Time left
+- | -------- | ------- | -- | ---------
+0 | ENCRYPTED / The.Walking.Dead.S05E01.Pre-Air.WEBRip.x264.AAC | 362 | 460 | 0:00:00
+1 | Defiance.S02E12-E13.HDTV.x264-KILLERS | 561 | 782 | 0:00:00
+2 | Under.the.Dome.S02E10.HDTV.x264-LOL | 214 | 302 | 0:00:00
+3 | Extant.S01E11.HDTV.x264-LOL | 291 | 291 | 0:00:00
+4 | Boardwalk.Empire.S05E01.HDTV.x264-KILLERS | 560 | 560 | 0:00:00
+5 | Extant.S01E12.HDTV.x264-LOL | 286 | 286 | 0:00:00
+6 | Haven.S05E01.HDTV.x264-KILLERS | 334 | 411 | 0:00:00
+7 | Doctor.Who.2005.S08E04.HDTV.x264-TLA | 304 | 304 | 0:00:00
+8 | Shortbus 2006 AVC _thebananafish_.cp(tt0367027) | 669 | 783 | 0:00:00
+9 | Suits.S04E08.HDTV.x264-KILLERS | 30 | 310 | 0:00:00
+10 | The.Walking.Dead.S03E01.BDRip.X264-DEMAND | 499 | 499 | 0:00:00
+11 | Terra.Nova.S01E11.DVDRip.XviD-DEMAND | 430 | 430 | 0:00:00
+12 | Arrow.S02E05.BDRip.x264-DEMAND | 414 | 414 | 0:00:00
+13 | Arrow.S02E04.BDRip.x264-DEMAND | 364 | 364 | 0:00:00
+14 | Arrow.S02E03.BDRip.x264-DEMAND | 387 | 387 | 0:00:00
+15 | The.Frozen.Ground.2013.1080p.BluRay.x264.YIFY.cp(tt2294629) | 1746 | 1746 | 0:00:00
+16 | 13 assassinss20100french MKV H264 HDrip Team Chez Titine.cp(tt1436045) | 1581 | 1582 | 0:00:00
+17 | Extant.S01E07E08.HDTV.x264-LOL | 503 | 503 | 0:00:00
+18 | Extant.S01E09E10.HDTV.x264-LOL | 591 | 591 | 0:00:00
+19 | Extant.S01E01.HDTV.x264-LOL | 253 | 253 | 0:00:00
+20 | Extant.S01E02.HDTV.x264-LOL | 259 | 259 | 0:00:00
+21 | Extant.S01E03.HDTV.x264-LOL | 322 | 322 | 0:00:00
+22 | Extant.S01E04.HDTV.x264-LOL | 291 | 291 | 0:00:00
+23 | Extant.S01E05.HDTV.x264-LOL | 273 | 273 | 0:00:00
+24 | Extant.S01E06.HDTV.x264-LOL | 288 | 288 | 0:00:00
--- trackmedia.js
+++ trackmedia.js
@@ -1,60 +1,157 @@
-#!/usr/bin/env node
-
-var fs = require('fs'),
+let fs = require('fs'),
 	moment = require('moment'),
-	async = require('async');
+	async = require('async'),
+	request = require('request');
 
 // base globals
-var username = process.env.USER;	
-var listsDir = '/home/' + username + '/Dropbox/_global/_live_lists/',
+const username = process.env.USER;	
+const listsDir = '/home/' + username + '/Dropbox/_global/_live_lists/',
 	torrentsComplete = '/media/media02/downloads/complete/',
-	ignorelist = [ '.DS_Store','._.DS_Store' ];
+	torrentsIncomplete = '/media/media02/downloads/',
+	staticMedia = '/home/' + username + '/.all_media_symlinks/',
+	ignorelist = [ '.DS_Store','._.DS_Store', 'complete' ],
+	SBUrl = 'http://localhost:8081/api/',
+	SBQuery = '?cmd=shows&sort=name',
+	sabNzbdUrl = 'http://localhost:8080/api?mode=qstatus&output=json&apikey=',
+	keyPath = '/home/' + username + '/bin/';
 
 /****************
  * EXISTING MEDIA
  ****************/
 
-// rtorrent downloads
-fs.readdir(torrentsComplete, function(err, folders) {
-	if (err) throw err;
-	var listName = 'completed.md',
-		md = '',
-		title = '# Live Complete List',
-		timeWritten = 'Updated: __' + moment().format('MMMM Do YYYY, h:mm:ss a') + '__';
+function crawlDirs(rootDir, listName) {
+	fs.readdir(rootDir, function(err, folders) {
+		if (err) throw err;
+		let	md = '',
+			title = '# Live Complete List',
+			timeWritten = 'Updated: __' + moment().format('MMMM Do YYYY, h:mm:ss a') + '__';
 
-	md = md +
-		title + '\n\n' +
-		timeWritten + '\n\n';
+		md = md +
+			title + '\n\n' +
+			timeWritten + '\n\n';
+
+		fs.writeFile(listsDir + listName, md, function(err) {
+			if (err) throw err;
+			async.each(folders, function(folder, callback) {
+				// do not use ignored files
+				if (!( ignorelist.indexOf(folder) > -1 )) {
+					fs.readdir(rootDir + folder + '/', function(err, files) {
+						// only print stuff out if folder has files
+						if (files.length) {
+							md = '';
+							md = md +
+								'\n## ' + folder + '\n\n' +
+								'~ | Title\n' +
+								'- | -----\n';
+
+							for (let i in files) {
+								md = md +
+									i + ' | ' +
+									files[i] + '\n';
+							}
+							fs.appendFile(listsDir + listName, md, function(err) {
+								if (err) throw err;
+								callback();
+							});
+						}					
+					});
+				}
+			}, function(err) {
+				if (err) throw err;	
+			});
+		});
+	});
+
+}
+
+// complete torrent downloads
+crawlDirs(torrentsComplete, 'torrents_complete.md');
+
+// all stored media
+crawlDirs(staticMedia, 'all_media.md');
+
+/**************
+ * TV SHOW STATS
+****************/
+
+function processSB(listName, callback) {
+	fs.readFile(keyPath + 'secretSickBeardKey', 'utf8', function(err, apiKey) {
+		if (err) throw err;
+		let fullUrl = SBUrl + apiKey + '/' + SBQuery;
+		fullUrl =  fullUrl.replace(/(\r\n|\n|\r)/gm,'');
+		request(fullUrl, function(error, response, shows) {
+			if (!error && response.statusCode == 200) {
+				callback(listName, shows);
+			}
+		});
+	});
+}
+
+processSB('tv_show_stats.md', function(listName, shows){
+	shows = JSON.parse(shows);
+	let md = '',
+	title = '# Live TV Shows Stats List\n\n',
+	timeUpdated = 'Updated: __' + moment().format('MMMM Do YYYY, h:mm:ss a') + '__\n\n',
+	tableHeadings = '~ | Show Name | Quality | Network | Status | Next Ep Airdate\n',
+	tableSeperators = '- | --------- | ------- | ------- | ------ | ---------------\n',
+	count = 0;
+
+	md = md + title + timeUpdated + tableHeadings + tableSeperators;
+
+	for (let i in shows.data) {
+		md = md + 
+			count + ' | ' +
+			shows.data[i].show_name + ' | ' +
+			shows.data[i].quality + ' | ' +
+			shows.data[i].network + ' | ' +
+			shows.data[i].status + ' | ' +
+			shows.data[i].next_ep_airdate + '\n';
+		count++;
+	}	
 
 	fs.writeFile(listsDir + listName, md, function(err) {
 		if (err) throw err;
-		async.each(folders, function(folder, callback) {
-			// do not use ignored files
-			if (!( ignorelist.indexOf(folder) > -1 )) {
-				fs.readdir(torrentsComplete + folder + '/', function(err, files) {
-					// only print stuff out if folder has files
-					if (files.length) {
-						md = '';
-						md = md +
-							'\n## ' + folder + '\n\n' +
-							'~ | Title\n' +
-							'- | -----\n';
-
-						for (var i in files) {
-							md = md +
-								i + ' | ' +
-								files[i] + '\n';
-						}
-						fs.appendFile(listsDir + listName, md, function(err) {
-							if (err) throw err;
-							callback();
-						});
-					}					
-				});
-			}
-		}, function(err) {
-			if (err) throw err;	
-		});
 	});
 });
 
+/* ********
+ * DOWNLOADS
+ * ********/
+
+// torrents incomplete
+crawlDirs(torrentsIncomplete, 'torrents_incomplete.md');
+
+function processSABnzdb(listName, callback) {
+	fs.readFile(keyPath + 'secretSabnzbdKey', 'utf8', function(err, apiKey) {
+		if (err) throw err;
+		let fullUrl = sabNzbdUrl + apiKey;
+		fullUrl =  fullUrl.replace(/(\r\n|\n|\r)/gm,'');
+		request(fullUrl, function(error, response, sabData) {
+			if (!error && response.statusCode == 200) {
+				callback(listName, sabData);
+			}
+		});
+	});
+}
+
+processSABnzdb('sabnzdb_incomplete.md', function(listName, sabData){
+	sabData = JSON.parse(sabData);
+	
+	// write to SABnzbd
+	let md = "# Downloading\n\n" + "## SABnzbd `( " + sabData.jobs.length + " )`\n\n" + 
+					"~ | Filename | MB left | MB | Time left\n" + 
+					"- | -------- | ------- | -- | ---------\n";
+
+	for (let x in sabData.jobs) {
+		md = md + 
+				x + " | " +
+				sabData.jobs[x].filename + " | " +
+				Math.round(sabData.jobs[x].mbleft) + " | " +
+				Math.round(sabData.jobs[x].mb) + " | " +
+				sabData.jobs[x].timeleft + "\n";
+	}
+
+	fs.writeFile(listsDir + '/' + listName, md, function(err){ 
+		if (err) throw err;
+	});
+});
